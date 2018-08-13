a38f901c77d29acc14842f72080ba7206165663c
Ahmed Sameh <ahmedsameh@Ahmeds-MacBook-Pro.local>
2018-08-13 19:41:09 +0300

    added sample page

---
+++ js/sample.js
@@ -0,0 +1,40 @@
+new WOW().init();
+
+$('document').ready(function() {
+  $("#dropdown").hide();
+
+//Bookmark control
+  $(".bookmark").click(function() {
+  console.log("yo");
+  $("#dropdown").show();
+  $(".bookmark").hide();
+});
+
+// ---------------------
+//Modal for image popup
+
+var modal = document.getElementById('myModal');
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+
+$('.myImg').click(function() {
+  modal.style.display = "block";
+  document.getElementById('bookmark').style.display = 'none';
+  var newSrc = this.src;
+  modalImg.attr('src', newSrc);
+  captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// -------------------------
+
+
+});
