7681694c7ccf5049893e3e76be7b4f6a319c5eaf
Ahmed Sameh <ahmedsameh@Ahmeds-MacBook-Pro.local>
2019-08-09 22:03:36 +0300

    more bunnies, fixed alignment

--- jstest.js
+++ jstest.js
@@ -1,7 +1,5 @@
 
-
 $(document).ready(function () {
-
 
     $(window).scroll(function () {
       console.log($(this).scrollTop());
@@ -20,3 +18,27 @@ $(document).ready(function () {
         };
     });
 });
+
+
+$(window).scroll(function () {
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop < 200) {
+      fontsize = 30;
+      rotate = 0
+  } else if (scrollTop > 800) {
+      fontsize = 20;
+      rotate = -90
+  } else {
+      fontsize = 30 - 10 * (((scrollTop - 200)) / 600);
+      rotate = -90  * (((scrollTop - 200)) / 600);
+  }
+
+  var breadcrumb = document.getElementById('dynamic');
+  if(breadcrumb.classList.contains('size-only')) {
+    rotate = 0;}
+
+  $('#dynamic').css({
+      'font-size': fontsize + "px",
+      'transform': "rotate(" +rotate + "deg)"
+  });
+})
